65dcfa4a75215d2582b48463877362a7c36edc1b
Robert Hogan <rhogan7@gmail.com>
2018-03-06 20:51:30 -0800

    Finished creating table and created Table Header

--- js/leaderboard.js
+++ js/leaderboard.js
@@ -1,36 +1,53 @@
 const container = document.querySelector('container');
 
-let leaderBoard = ["apple", "basdf", "casdf", "dasdf", "eadsf", "fasdf", "gasdf", "hasdf", "iasdf", "jasdf"];
+let leaderBoard = [	"Robert", "3", "16", "0:25" ];
 const leaderBoardHeaders = ["Name", "Total Stars", "Total Moves", "Time"];
 
 function makeGrid() {
 
 var table = document.createElement('table');
 
-	for (var i = 0; i < totalScores; i++) {
+	var tHead = document.createElement('thead');
+	var tr = document.createElement("tr");
 
+	for(let i = 0; i < leaderBoardHeaders.length; i++) {
+		let th = document.createElement('th');
+		let thText = document.createTextNode('');
+		th.appendChild(thText);
+		tr.appendChild(th);
+		tHead.appendChild(tr);
+		table.appendChild(tHead);
+	}
+
+	var tBody = document.createElement('tbody');
+	
+	for (var i = 0; i < leaderBoard.length + 1; i++) {
 	    var tr = document.createElement('tr');
 	    tr.id = ("row" + i);   
 
-	    for(let j = 0; j < totalScores; j++) {
-
-		    var td1 = document.createElement('td');
+	   	for(let j = 0; j < leaderBoardHeaders.length; j++) {
+	
+		    var td = document.createElement('td');
 		    var text1 = document.createTextNode('');
-		    td1.appendChild(text1);
-	    }
-
-	    tr.appendChild(td1);
-	    table.appendChild(tr);
+	    	td.appendChild(text1);
+	    	tr.appendChild(td);
+    	}
+	    tBody.appendChild(tr);
+	    table.appendChild(tBody);
 	}
 	document.body.appendChild(table);
 };
 
 function fillTables() {
 
+	let ths = document.querySelectorAll("th");
+	for(let i = 0; i < leaderBoardHeaders.length; i++) {
+		ths[i].innerHTML = leaderBoardHeaders[i];
+	}
+
 	let tds = document.querySelectorAll("td");
 	for(let i = 0; i < leaderBoard.length; i++) {
 		tds[i].innerHTML = leaderBoard[i];
-		console.log(tds[i]);
 	}
 }
 
